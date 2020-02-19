d1208fec926c020d6259b8c4522819910fbf20e6
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-02-19 17:58:27 +0000

    Create equation_parent.py
    -Contains parent class "equation"
    -Commented test child class

---
+++ equation_parent.py
@@ -0,0 +1,25 @@
+from abc import abstractmethod
+class equation:
+    def __init__(self,name):
+        self.name=name
+        
+    @abstractmethod
+    def __call__(self):
+        pass
+    
+    def __repr__(self):
+        return self.name+"="+str(self.__call__())
+
+"""
+class add(equation):
+    
+    def __init__(self,name):
+        super().__init__(name)
+    def __call__(self):
+        return self.i
+    
+    
+x=add("2+2",4)
+
+print(x)
+"""
