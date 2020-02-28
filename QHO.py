9884c5ae63a7794b7351dafddcdd26467846bf7c
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-02-28 15:45:28 +0000

    Added testing functions

--- QHO.py
+++ QHO.py
@@ -7,24 +7,40 @@ class quantum_harmonic_oscilator:
         self.delta_x=delta_x
         self.energy=trial_energy
         self.mass=mass
-        self.wavefunction=np.array([0,10E-10])
+        self.position_array=np.array([start_position])
+        self.wavefunction=np.array([0,1E-4])
         
         
     #Numerov method------------------------------------------------------
         
     def g_n(self,potential):
-        return (2*self.mass/const.hbar**2)*(self.energy-potential)
+       # print("V = {}".format(potential))
+     #   print(potential,(2*self.mass/(const.hbar**2))*(self.energy-potential))
+      #  print("---------------------------------")
+        return (2*self.mass/(const.hbar**2))*(self.energy-potential)
    
     def f_n(self,potential):
-        return 1+self.g_n(potential)*(self.delta_x**2/12)
+        return 1+self.g_n(potential)*((self.delta_x**2)/12)
     
     def next_wavevalue(self,potential0,potential_minus1,potential_minus2): #potential0 for current value of x
+        """
+        print(format((12-10*self.f_n(potential_minus1)) * self.wavefunction[-1],"1.1E"))
+        print(format(self.f_n(potential_minus2) * self.wavefunction[-2],"1.1E"))
+        print(format(self.f_n(potential0),"1.1E"))
+        print("----------------------------------------------------------------------")
+        """
         psi=((12-10*self.f_n(potential_minus1)) * self.wavefunction[-1]-  self.f_n(potential_minus2) * self.wavefunction[-2])    /self.f_n(potential0)
-        print(psi)
+      #  psi=2*self.wavefunction[-1]*(1-5*self.delta_x)
+        #print(psi)
         self.wavefunction=np.append(self.wavefunction,psi)
+    #    print (psi)
         #print(self.wavefunction)
         return psi
     #--------------------------------------------------------------------
+
+    def next_wavevalue2(self,potential0,potential_minus1,potential_minus2):
+        return
+
 
 """
 x=quantum_harmonic_oscilator(0.5, 1, 0.005)
