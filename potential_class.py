92cb8be89d86f607da4a08fb4da997c9a9a991ad
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-02-28 15:45:13 +0000

    Update potential_class.py

--- potential_class.py
+++ potential_class.py
@@ -8,21 +8,24 @@ class potential:
         self.upper_limit=upper_limit
             
     def V(self,position):
+      #  print(position)
         infinity_approximation=1E100
         x=symbols('x')
         if self.form=="harmonic":            
             V=0.5*self.k*x**2
         if self.form=="ISW": #infinte square well   
-            V=0
+            V=0*x
         
         if position<=self.lower_limit or position>=self.upper_limit:
-            return infinity_approximation
+         #   print("∞ {}".format(position))
+            return V.subs(x,position)#infinity_approximation
         else:
+           # print("finite potential")
             return V.subs(x,position)
         
         
 """
-x=potential()
+x=potential(-1,1)
 
-print(x.V(12))
+print(x.V(-1))
 """
