2abff19aee48fc89b52e5b3768b101f166d6bd76
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-03-07 16:45:35 +0000

    Added docstring

--- error.py
+++ error.py
@@ -1,5 +1,18 @@
 import sys
 
 def error(message):
+    """
+    Displays error message and ends the current script
+
+    Parameters
+    ----------
+    message : str
+        Error message to be displayed.
+
+    Returns
+    -------
+    None.
+
+    """
     print("ERROR: "+message)
     sys.exit()
