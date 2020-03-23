64964222bdb8103d6f758a1f483db1e4d58e9655
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-03-23 13:54:00 +0000

    Adjusted var naming for clarity

--- multiprocess_generator.py
+++ multiprocess_generator.py
@@ -3,15 +3,15 @@ import multiprocessing as mp
 import time
 
 start=time.time()
-simulations = 5 #Number of simulations to run (NOTE: Each successive simulation takes longer to run)
+simulations = 8 #Number of simulations to run (NOTE: Each successive simulation takes longer to run)
 
 
 if __name__ == "__main__":  # Allows for the safe importing of the main module
     print("There are {} logical processors on this machine".format(mp.cpu_count()))
     print("Running {} simulations...".format(simulations))
     
-    number_processes = mp.cpu_count()
-    pool = mp.Pool(number_processes)
+    max_processes = mp.cpu_count()
+    pool = mp.Pool(max_processes)
     processes = pool.map_async(wfg.run, range(1,simulations+1))
     pool.close()
     pool.join()
