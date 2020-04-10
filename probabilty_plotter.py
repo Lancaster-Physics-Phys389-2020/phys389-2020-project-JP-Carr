01afd425cdcd9d32692332253cb06df7a902792f
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-10 13:54:25 +0100

    Added try for loading csvs and fixed labels

--- probabilty_plotter.py
+++ probabilty_plotter.py
@@ -2,10 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import listdir
 import pandas as pd
+from error import error
 
 
-
-wave_function_csv=listdir("wavefunctions")[0]  #temporary
+try:
+    wave_function_csv="wavefunctions_N=1000.csv"#listdir("wavefunctions")[0]  #temporary
+    
+except:
+    error("Unable to load"+str(wave_function_csv))
 #print(str(wave_function_csv))
 data=pd.read_csv("wavefunctions\\"+str(wave_function_csv))
 N=len(data)
@@ -13,7 +17,7 @@ N=len(data)
 x_array=np.linspace(-1/2,1/2,N)
 
 
-plt.figure("Wavefunctions (N={})".format(N))
+plt.figure("Probability (N={})".format(N))
     
 plt.xlabel("x")
 plt.ylabel("P(x)")
--- wavefunction_plotter.py
+++ wavefunction_plotter.py
@@ -2,10 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os import listdir
 import pandas as pd
+from error import error
 
 
 
-wave_function_csv=listdir("wavefunctions")[0]  #temporary
+
+#print(wave_function_csv)
+try:
+    wave_function_csv="wavefunctions_N=1000.csv"#listdir("wavefunctions")[0]  #temporary
+    
+except:
+    error("Unable to load"+str(wave_function_csv))
 data=pd.read_csv("wavefunctions\\"+str(wave_function_csv))
 N=len(data)
 
@@ -15,7 +22,7 @@ x_array=np.linspace(-1/2,1/2,N)
 plt.figure("Wavefunctions (N={})".format(N))
     
 plt.xlabel("x")
-plt.ylabel("ψ")
+plt.ylabel("ψ(x)")
   
 i=1 
 while 1:
