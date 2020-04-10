707ddbba5d9747b07f23c67bea9abbfce2425c31
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-10 10:57:13 +0100

    Fixed labels, added ticks

--- probabilty_plotter.py
+++ probabilty_plotter.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 
 wave_function_csv=listdir("wavefunctions")[0]  #temporary
+#print(str(wave_function_csv))
 data=pd.read_csv("wavefunctions\\"+str(wave_function_csv))
 N=len(data)
 
@@ -15,7 +16,7 @@ x_array=np.linspace(-1/2,1/2,N)
 plt.figure("Wavefunctions (N={})".format(N))
     
 plt.xlabel("x")
-plt.ylabel("ψ")
+plt.ylabel("P(x)")
   
 i=1 
 while 1:
@@ -24,12 +25,12 @@ while 1:
         integral=np.trapz(prob,x=x_array)
         prob_norm=prob/integral
         print(np.trapz(prob_norm,x=x_array))
-
+        
         plt.plot(x_array,prob_norm, label="n="+str(i))
         i+=1
     except:
         break
     
-    
+plt.tick_params(which='both',direction='in',right=True,top=True)
 plt.legend()
 plt.show()
