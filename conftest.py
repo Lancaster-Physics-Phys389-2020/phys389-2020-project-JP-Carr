ec37d8e8fd67693facdf7b9cb527bd88d8d36250
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-10 18:29:48 +0100

    Added modules

--- conftest.py
+++ conftest.py
@@ -3,3 +3,5 @@ import pytest
 def execute_before_any_test():
     from potential_class import potential
     from QHO import quantum_harmonic_oscilator 
+    import wavefunction_generator 
+    from analytical_E_levels import analytical_E
