b3641af6b9d4038cb4a34b6026b3f8d76052964b
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-12 19:17:16 +0100

    Function now normalised and added comparison to analytical psi

--- wavefunction_plotter.py
+++ wavefunction_plotter.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 from os import listdir
 import pandas as pd
 from error import error
+from probability_plotter import normalise
+from analytical_psi import psi as analytical_psi
+import math
 
-
-
+norm=True
+comparison=True
 
 #print(wave_function_csv)
 try:
@@ -27,11 +30,31 @@ plt.ylabel(r"$\psi(\tilde{x})$")
 i=1 
 while 1:
     try:
-        plt.plot(x_array,data[str(i)], label="n="+str(i))
+        psi=data[str(i)]
+        norm_psi=psi*normalise(data[str(i)], x_array)
+        if norm==True:
+            plt.plot(x_array,norm_psi, label="n="+str(i))
+        else:
+            plt.plot(x_array,psi, label="n="+str(i))
         i+=1
     except:
         break
     
 plt.tick_params(which='both',direction='in',right=True,top=True)
 plt.legend()
+
+if comparison==True:
+    plt.figure("Wavefunctions comparison")
+    i=1 
+    while i==1:
+        try:
+            psi_diff=np.array(data[str(i)]*normalise(data[str(i)], x_array) /analytical_psi(x_array, i, 1))
+         #   print(np.delete(psi_diff, [999]))
+            plt.plot(np.delete(x_array,[999]),np.log10(np.delete(psi_diff, [999])),label="n="+str(i))
+            i+=1
+        except:
+            break
+plt.xlabel(r"$\tilde{x}$")
+plt.ylabel(r"$log(\psi_{numerical}/\psi_{analytical})$")
+#plt.legend()  
 plt.show()
