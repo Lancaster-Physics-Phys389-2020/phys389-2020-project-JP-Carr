0eb171468f82f055d96d16e7a70204bcfee0505b
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-17 17:35:38 +0100

    Moved time import to fix multiprocessing

--- wavefunction_generator.py
+++ wavefunction_generator.py
@@ -4,6 +4,7 @@ import numpy as np
 import copy
 import pandas as pd
 from error import error
+import time
 
 
 well_length=1  #length of the potential well
@@ -16,9 +17,8 @@ if __name__=="__main__":
     """
     Setup if script is being run directly
     """
-    import time
     start=time.time() #begins timing
-    N=round(50)  #number of integration steps
+    N=round(50*2)  #number of integration steps
     x_array=np.linspace(-well_length/2,well_length/2,N) #array of positions
     V=potential(N) #potential object
 
