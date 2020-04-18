fd6a3692152ce7fb447936e61df4ce2327397713
JP-Carr <j.p.carr@lancaster.ac.uk>
2020-04-18 13:37:03 +0100

    Added clarification on the number of simulations

--- linear_timer.py
+++ linear_timer.py
@@ -3,9 +3,10 @@ import numpy as np
 """
 Times "wavefunction_generator.py" at different values of N
 """
+simulations=10 # number of simulations to be run (NOTE: this can take a long time for a large number of simulations
 
 store=[[],[]]
-for i in range(1,31):
+for i in range(1,simulations+1):
     N,t=run(i)
     print(N,t)
     store[0].append(N)
